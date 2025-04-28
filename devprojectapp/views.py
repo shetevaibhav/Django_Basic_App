67b099bc8da53dceb3bbec1d579dda155ccc9bdf
Vaibhav Shete <vaibhav@intellifysolutions.com>
2025-04-28 17:05:45 +0530

    changes in view file

--- devprojectapp/views.py
+++ devprojectapp/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello this is django app from producation!")
+    return HttpResponse("Hello this is django app from producation and remove whitenoise!")
